700e491eb98185715486c5e1b3e43d37c0ba9a85
Ceyhun Turkmen <ctrkmn64@gmail.com>
2025-04-26 10:45:27 +0300

    config

--- client/src/features/services/axiosInstance.js
+++ client/src/features/services/axiosInstance.js
@@ -2,7 +2,6 @@
 import axios from 'axios';
 
 const API_URL = `${import.meta.env.VITE_API_PRODUCTION_URL}/auth`;
-console.log(API_URL);
 
 const axiosInstance = axios.create({
     baseURL: API_URL,//baseUrl belirliyoruz
