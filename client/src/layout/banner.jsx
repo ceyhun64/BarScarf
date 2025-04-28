62014dd7eab8d4517f95a541677800bd2e600e75
Ceyhun Turkmen <ctrkmn64@gmail.com>
2025-04-28 15:01:15 +0300

    banner3

--- client/src/layout/banner.jsx
+++ client/src/layout/banner.jsx
@@ -3,28 +3,29 @@ import { Link } from "react-router-dom";
 import bannerImage from "../assets/images/banner.jpg";
 
 export default function Banner() {
-    return (
-        <div className="banner" style={{ marginTop: "20px" }}>
-            <Link to="/products" style={{ textDecoration: "none" }}>
-                <div
-                    style={{
-                        backgroundImage: `url(${bannerImage})`,
-                        backgroundSize: "cover",
-                        backgroundPosition: "center",
-                        width: "100%",
-                        height: "500px",
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        color: "white",
-                        fontSize: "2rem",
-                        fontWeight: "bold",
-                        textShadow: "2px 2px 8px rgba(0,0,0,0.7)",
-                        borderRadius: "10px",
-                    }}
-                >
-                </div>
-            </Link>
+  return (
+    <div className="banner" style={{ marginTop: "20px" }}>
+      <Link to="/products" style={{ textDecoration: "none" }}>
+        <div
+          style={{
+            width: "100%",
+            overflow: "hidden",
+            borderRadius: "10px",
+            position: "relative",
+          }}
+        >
+          <img
+            src={bannerImage}
+            alt="Banner"
+            style={{
+              width: "100%",
+              height: "auto", // Genişliğe göre yükseklik ayarlanır
+              display: "block",
+            }}
+          />
+       
         </div>
-    );
+      </Link>
+    </div>
+  );
 }
