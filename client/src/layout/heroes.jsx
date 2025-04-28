66940ceaded49bdca04f79eb70d120edb8e537dc
Ceyhun Turkmen <ctrkmn64@gmail.com>
2025-04-28 15:23:17 +0300

    heroes2

--- client/src/layout/heroes.jsx
+++ client/src/layout/heroes.jsx
@@ -7,44 +7,44 @@ import heroesImage4 from "../assets/images/heroes4.jpg";
 const images = [heroesImage, heroesImage2, heroesImage3, heroesImage4];
 
 export default function Heroes() {
-    const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-        }, 3000); // 3 saniyede bir değişsin
-        return () => clearInterval(interval);
-    }, []);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000); // 3 saniyede bir değişsin
+    return () => clearInterval(interval);
+  }, []);
 
-    return (
+  return (
+    <div
+      className="heroes-slider"
+      style={{
+        width: "100%",
+        aspectRatio: "16/9", // Oranı koru: genişlik değişirse yükseklik de otomatik ayarlanır
+        overflow: "hidden",
+        position: "relative",
+        borderRadius: "10px",
+        marginBottom: "20px",
+      }}
+    >
+      {images.map((image, index) => (
         <div
-            className="heroes-slider"
-            style={{
-                width: "100%",
-                height: "500px",
-                overflow: "hidden",
-                position: "relative",
-                borderRadius: "10px",
-                marginBottom: "20px",
-            }}
-        >
-            {images.map((image, index) => (
-                <div
-                    key={index}
-                    style={{
-                        backgroundImage: `url(${image})`,
-                        backgroundSize: "cover",
-                        backgroundPosition: "center",
-                        width: "100%",
-                        height: "500px",
-                        position: "absolute",
-                        top: 0,
-                        left: 0,
-                        opacity: currentIndex === index ? 1 : 0,
-                        transition: "opacity 1s ease-in-out",
-                    }}
-                />
-            ))}
-        </div>
-    );
+          key={index}
+          style={{
+            backgroundImage: `url(${image})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            opacity: currentIndex === index ? 1 : 0,
+            transition: "opacity 1s ease-in-out",
+          }}
+        />
+      ))}
+    </div>
+  );
 }
